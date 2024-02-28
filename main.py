71a38a76b573c35a68930f671bd62fbe9c9f9cee
TerminalC3 <keerthikumar.m10@gmail.com>
2024-02-28 20:55:09 +0530

    Printing result at the end

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cv2
+from PIL import Image
+import numpy as np
 
-file_path = 'video.mp4'
+file_path = 'short-video.mp4'
 
 vid = cv2.VideoCapture(file_path)
 
@@ -8,3 +10,13 @@ height = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
 width = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
 
 print(f'Height: {height}, Width: {width}')
+
+frames = []
+
+while True:
+    frame = vid.read()
+    if frame[0] == False:
+        break
+    frames.append(frame)
+
+print(frames)
