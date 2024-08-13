e8c2cd71ec6ee416e481c88d9667401104ee3435
keerthikmr <keerthikumar.m10@gmail.com>
2024-08-13 22:38:27 +0530

    Get RGB values and store it in a file

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import cv2
 import numpy as np
 
-file_path = 'short-video.mp4'
+video_path = 'output.mp4'
 
-vid = cv2.VideoCapture(file_path)
-
-height = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
-width = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
-
-print(f'Height: {height}, Width: {width}')
-
-frames = []
+video_capture = cv2.VideoCapture(video_path)
 
 while True:
-    frame = vid.read()
-    if frame[0] == False:
-        break
-    frames.append(frame)
+    ret, frame = video_capture.read()
 
-print(frames)
+    if not ret:
+        break
+
+    # Default is BGR, switch to RGB
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    height, width, _ = frame_rgb.shape
+
+    file = open("output.txt", "w")
+
+    # Get RGB values of each pixel in each frame and save it to a file
+    for y in range(height):
+        for x in range(width):
+
+            r, g, b = frame_rgb[y, x]
+
+            file.write((str(r) + " "+ str(g)+ " "+ str(b)) + "\n")
+
+video_capture.release()
