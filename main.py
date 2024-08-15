18d1b9eda0ec9bc3667ab17712e391b84d9fedbd
keerthikmr <keerthikumar.m10@gmail.com>
2024-08-15 18:09:32 +0530

    Convert rgb values to yuv and store them in a file

--- main.py
+++ main.py
@@ -5,25 +5,31 @@ video_path = 'output.mp4'
 
 video_capture = cv2.VideoCapture(video_path)
 
-while True:
-    ret, frame = video_capture.read()
+def get_rgb():
+    while True:
+        ret, frame = video_capture.read()
 
-    if not ret:
-        break
+        if not ret:
+            break
 
-    # Default is BGR, switch to RGB
-    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # Default is BGR, switch to RGB
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    height, width, _ = frame_rgb.shape
+        height, width, _ = frame_rgb.shape
 
-    file = open("output.txt", "w")
+        file = open("output.txt", "w")
 
-    # Get RGB values of each pixel in each frame and save it to a file
-    for y in range(height):
-        for x in range(width):
+        # Get RGB values of each pixel in each frame and save it to a file
+        for a in range(height):
+            for b in range(width):
 
-            r, g, b = frame_rgb[y, x]
+                r, g, b = frame_rgb[a, b]
 
-            file.write((str(r) + " "+ str(g)+ " "+ str(b)) + "\n")
+                y = 0.299*r + 0.587*g + 0.114*b
+                u = -0.169*r - 0.331*g + 0.449*b + 128
+                v = 0.499*r - 0.418*g - 0.0813*b + 128
 
-video_capture.release()
+                file.write((str(r) + " "+ str(g)+ " "+ str(b)) + "\n")
+
+    video_capture.release()
+
