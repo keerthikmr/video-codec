0d34a9bccbde08855e1bfaad0377080955b71c96
keerthikmr <keerthikumar.m10@gmail.com>
2024-08-15 19:54:13 +0530

    Get yuv values of each pixel

--- main.py
+++ main.py
@@ -1,35 +1,48 @@
 import cv2
 import numpy as np
+import time
 
 video_path = 'output.mp4'
 
 video_capture = cv2.VideoCapture(video_path)
 
-def get_rgb():
+ret, frame = video_capture.read()
+
+# Stores YUV values of the entire video
+def get_yuv():
+    
+    ret, frame = video_capture.read()
+    
+    height, width, _ = frame.shape
+
     while True:
         ret, frame = video_capture.read()
 
         if not ret:
             break
-
-        # Default is BGR, switch to RGB
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        height, width, _ = frame_rgb.shape
-
-        file = open("output.txt", "w")
+        
+        yuv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2YUV)
+        
+        # Stores YUV values of the frame
+        Y, U, V = [], [], []
 
         # Get RGB values of each pixel in each frame and save it to a file
-        for a in range(height):
-            for b in range(width):
+        for i in range(height):
+            for j in range(width):
 
-                r, g, b = frame_rgb[a, b]
+                y, u, v = yuv_frame[i, j]
 
-                y = 0.299*r + 0.587*g + 0.114*b
-                u = -0.169*r - 0.331*g + 0.449*b + 128
-                v = 0.499*r - 0.418*g - 0.0813*b + 128
-
-                file.write((str(r) + " "+ str(g)+ " "+ str(b)) + "\n")
+                Y.append(y)
+                U.append(u)
+                V.append(v)
 
     video_capture.release()
 
+
+start_time = time.time()
+
+get_yuv()
+
+end_time = time.time()
+
+print(end_time-start_time)
